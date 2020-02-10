1c9bd36dc812a93b5d3e78d8e6def57ff89bbfbf
khs1994 <khs1994@khs1994.com>
2020-02-10 12:43:41 +0800

    Sync from pcit-ce/pcit@2d7a0e936e98cd54ca6b087ac0053d7ca9adeb54 by PCIT

---
+++ src/WebhooksServer.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCIT\Provider\AliyunDockerRegistry;
+
+use Exception;
+use PCIT\Provider\Interfaces\WebhooksServerInterface;
+
+/**
+ * 接收 Webhooks.
+ */
+class WebhooksServer implements WebhooksServerInterface
+{
+    /**
+     * @throws Exception
+     */
+    public function server(): void
+    {
+        \Log::info('receive aliyun_docker_registry webhooks');
+
+        $request = app('request');
+
+        // $content = file_get_contents('php://input');
+        $content = $request->getContent();
+
+        \Cache::store()->lpush('/pcit/webhooks', json_encode(['aliyun_docker_registry', 'push', $content]));
+    }
+}
